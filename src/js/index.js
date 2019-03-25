dd2c205a31b04c29e4e780ebf4a8d5ab082a28ac
Tommy <tommy.ollerenshaw@intouchnetworks.com>
2019-03-25 19:31:52 +0000

    update outs to inputs to allow user to choose amount

--- src/js/index.js
+++ src/js/index.js
@@ -3,61 +3,65 @@ class LoanCalculator {
         this.input = {
         loanAmount: document.getElementsByClassName('js-valueLoan')[0],
         annualInterest:document.getElementsByClassName('js-valueInterest')[0],
-        repaymentPeriod: document.getElementsByClassName('js-valueLoanPeriod')[0],
-        interest: document.getElementsByClassName('js-total-interest')[0]
+        repaymentPeriod: document.getElementsByClassName('js-valueLoanPeriod')[0]
     }    
 }
 
+updateResultsTitle(loanYears) {
+    document.getElementsByClassName('js-title')[0].innerHTML = `Your loan over ${loanYears} years`
+}
+
+displayCalculation(total, outputID) {
+    //Display the calculations
+    document.getElementsByClassName(`${outputID}`)[0].innerHTML= `£ ${total}`;
+}
+
+
+calculateMonthleyPayments(loanAmount, monthlyInterest, repaymentPeriod) {
+    var loanPerMonth = loanAmount / repaymentPeriod / 12;
+    var monthlypayment = loanPerMonth + monthlyInterest;
+
+    this.displayCalculation(monthlypayment.toFixed(2), 'js-monthly-payment')
+}
+
+calculateTotalPayments(loanAmount, totalInterest) {
+    var totalPayments = +loanAmount + +totalInterest;
+
+    this.displayCalculation(totalPayments.toFixed(2), 'js-total-payment')
+}
 
 calculateInterest(loanAmount, annualInterest, repaymentPeriod) {
     //Calulate interest
     var yearlyInterest = (loanAmount / 100 ) * annualInterest;
     var monthlyInterest = yearlyInterest / 12;
     var totalInterest = yearlyInterest * repaymentPeriod;
+
+    this.calculateMonthleyPayments(this.input.loanAmount.value, monthlyInterest, this.input.repaymentPeriod.value);
+    this.calculateTotalPayments(this.input.loanAmount.value, totalInterest);
+
     this.displayCalculation(totalInterest.toFixed(2), 'js-total-interest');
-    this.calculateMonthleyPayments(this.input.loanAmount.innerHTML, monthlyInterest, this.input.repaymentPeriod.innerHTML);
-    this.calculateTotalPayments(this.input.loanAmount.innerHTML, totalInterest);
 }
 
-calculateMonthleyPayments(loanAmount, monthlyInterest, repaymentPeriod) {
-    var loanPerMonth = loanAmount / repaymentPeriod / 12;
-    var monthlypayment = loanPerMonth + monthlyInterest;
-    this.displayCalculation(monthlypayment.toFixed(2), 'js-monthly-payment')
-}
-
-calculateTotalPayments(loanAmount, totalInterest) {
-    var totalPayments = +loanAmount + +totalInterest;
-    this.displayCalculation(totalPayments.toFixed(2), 'js-total-payment')
-}
-
-
-collectData(inputID, outputID) {
-//Collect data from form
+collectData() {
+    //Collect data from form
     var calculateButton = document.getElementsByClassName('js-calculate')[0];
 
-    var total;
-
     calculateButton.addEventListener('click', () => {
-        total = document.getElementsByClassName(`${inputID}`)[0].innerHTML;
-            this.calculateInterest(this.input.loanAmount.innerHTML, this.input.annualInterest.innerHTML, this.input.repaymentPeriod.innerHTML);
+            this.calculateInterest(this.input.loanAmount.value, this.input.annualInterest.value, this.input.repaymentPeriod.value);
+            this.updateResultsTitle(calculator.input.repaymentPeriod.value);
     })
 }
-    
-    
-displayCalculation(total, outputID) {
-    //Display the calculations
-    document.getElementsByClassName(`${outputID}`)[0].innerHTML= `£ ${total}`;
-}
+
     
 updateInput(inputID, outputClass) {
     //Update outputs with input from range slider
     var slider = document.getElementById(`${inputID}`),
         sliderOutput = document.getElementsByClassName(`${outputClass}`)[0];
 
-    sliderOutput.innerHTML = slider.value;
+    sliderOutput.value = slider.value;
 
     slider.addEventListener('input', () => {
-        sliderOutput.innerHTML = slider.value;
+        sliderOutput.value = slider.value;
     }, false);
 }
 }
@@ -67,4 +71,4 @@ const calculator = new LoanCalculator;
 calculator.updateInput('loanAmount', 'js-valueLoan');
 calculator.updateInput('annualInterest', 'js-valueInterest');
 calculator.updateInput('repaymentPeriod', 'js-valueLoanPeriod');
-calculator.collectData('js-valueLoan', 'js-total-loan');
+calculator.collectData();
