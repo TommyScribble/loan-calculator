e56c29518fe07be013b421b36e5d67b4a040aeac
Tommy <tommy.ollerenshaw@intouchnetworks.com>
2019-03-25 21:46:04 +0000

    connect totals to chart

--- src/js/chart.js
+++ src/js/chart.js
@@ -1,25 +1,38 @@
-google.charts.load("current", { packages: ["corechart"] });
+var calculateButton = document.getElementsByClassName('js-calculate')[0];
+var loan = 0;
+var interestInteger = 0;
+calculateButton.addEventListener('click', () => {
 
-google.charts.setOnLoadCallback(drawChart);
+    loan = document.getElementsByClassName('js-valueLoan')[0].value;
+    loanInteger = parseInt(loan, 10);
+    interest = document.getElementsByClassName('js-total-interest')[0].innerHTML.replace(/[^0-9.-]+/g,"");
+    interestSlice = interest.slice(0, 3);
+    interestInteger = parseInt(interestSlice, 10)
+    
+    google.charts.load("current", { packages: ["corechart"] });
+    
+    google.charts.setOnLoadCallback(drawChart);
 
-function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-        ['Payment type', 'Total (in pounds sterling)'],
-        ['Loan', 7500],
-        ['Interest', 750]
-    ]);
+    
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Payment type', 'Total (in pounds sterling)'],
+            ['Loan', loanInteger],
+            ['Interest', interestInteger]
+        ]);
 
-    var options = {
-        backgroundColor: 'transparent',
-        chartArea: {'width': '100%', 'height': '100%'},
-        legend: 'none',
-        pieSliceTextStyle: {
-            color: 'none',
-          },
-        pieHole: 0.8,
-        colors: ['#ffffff', '#2a72db'],
-    };
+        var options = {
+            backgroundColor: 'transparent',
+            chartArea: {'width': '100%', 'height': '100%'},
+            legend: 'none',
+            pieSliceTextStyle: {
+                color: 'none',
+            },
+            pieHole: 0.8,
+            colors: ['#ffffff', '#2a72db'],
+        };
 
-    var chart = new google.visualization.PieChart(document.getElementById('chart'));
-    chart.draw(data, options);
-}
+        var chart = new google.visualization.PieChart(document.getElementById('chart'));
+        chart.draw(data, options);
+    }
+})
